a610e4a331c18de0c543f51dcd3971eb46f7aa00
Thomas Bradley <bradley.tom.r@gmail.com>
2015-11-30 16:23:22 +0000

    Defines and implements oystercard balance variable

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,2 +1,10 @@
-Describe Oystercard do
+require 'oystercard'
+
+describe Oystercard do
+  subject(:oystercard) { described_class.new }
+  context '#balance' do
+    it 'had a default balance of zero' do
+      expect(oystercard.balance).to eq(described_class::DEFAULT_BALANCE)
+    end
+  end
 end
