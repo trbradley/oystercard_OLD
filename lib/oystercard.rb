3f281e5e8a6a5ec04339cfb283c2dde61670d0d7
Thomas Bradley <bradley.tom.r@gmail.com>
2015-12-01 12:40:08 +0000

    Ch10 complete. Charge on touch_out

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,7 +1,7 @@
 class Oystercard
   DEFAULT_BALANCE = 0
   MAXIMUM_BALANCE = 90
-  MINIMUM_BALANCE = 1
+  MINIMUM_CHARGE = 1
 
   attr_reader :balance, :in_journey
   alias_method :in_journey?, :in_journey
@@ -16,28 +16,29 @@ class Oystercard
     @balance += value
   end
 
-  def deduct(fare)
-    @balance -= fare
-  end
-
   def touch_in
     under_min_balance
     @in_journey = true
   end
 
-  def touch_out
+  def touch_out(fare)
+    deduct(fare)
     @in_journey = false
   end
 
   private
 
+  def deduct(fare)
+    @balance -= fare
+  end
+
   def max_balance_reached(x)
     fail "Maximum balance of #{MAXIMUM_BALANCE} exceeded" if
       balance + x > MAXIMUM_BALANCE
-      # TODO: CHECK BEST PRACTICE FOR SINGLE RESPONSIBILITY RE FAIL GUARD CLAUSES
+    # TODO: CHECK BEST PRACTICE FOR SINGLE RESPONSIBILITY RE FAIL GUARD CLAUSES
   end
 
   def under_min_balance
-    fail 'Insufficient balance' if balance < MINIMUM_BALANCE
+    fail 'Insufficient balance' if balance < MINIMUM_CHARGE
   end
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -11,7 +11,6 @@ describe Oystercard do
 
   context '#top_up' do
     it 'tops up the card by a value and returns the balance' do
-      oystercard.deduct(10)
       expect { oystercard.top_up(1) }.to change { oystercard.balance }.by 1
     end
   end
@@ -28,8 +27,8 @@ describe Oystercard do
     end
   end
 
-
   context 'Topped up to max balance' do
+    let(:fare) { described_class::MINIMUM_CHARGE }
     before do
       oystercard.top_up(described_class::MAXIMUM_BALANCE)
     end
@@ -39,12 +38,6 @@ describe Oystercard do
         max_balance = described_class::MAXIMUM_BALANCE
         expect { oystercard.top_up(1) }
           .to raise_error "Maximum balance of #{max_balance} exceeded"
-      end
-    end
-
-    context '#deduct' do
-      it 'deducts a fare from card' do
-        expect { oystercard.deduct 3 }.to change { oystercard.balance }.by(-3)
       end
     end
 
@@ -58,8 +51,13 @@ describe Oystercard do
     context '#touch_out' do
       it 'changes card to be not in use' do
         oystercard.touch_in
-        expect { oystercard.touch_out }.to change { oystercard.in_journey }
-          .to false
+        expect { oystercard.touch_out(fare) }
+          .to change { oystercard.in_journey }.to false
+      end
+
+      it 'deducts fare from balance' do
+        expect { oystercard.touch_out(fare) }.to change { oystercard.balance }
+          .by(-fare)
       end
     end
   end
